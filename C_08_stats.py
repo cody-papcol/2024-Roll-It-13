0cabe811c321b2f3a64fce149ff81af4171b8444
cody-papcol <164798036+cody-papcol@users.noreply.github.com>
2024-05-06 09:34:30 +1200

    added stats file

---
+++ C_08_stats.py
@@ -0,0 +1,10 @@
+# create lists to hold user and computer scores
+user_scores = []
+computer_scores = []
+
+# loop six times - for testing purposes, ask the user to enter
+# the score for the user and the computer for each round
+
+
+# calculate the lowest, highest and average scores
+# and display them.
